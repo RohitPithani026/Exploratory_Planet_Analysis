3f8a15a6becf1af146da2a9f616ce6cdca249fea
RohitPithani026 <rohitpithani13@gmail.com>
2025-03-20 23:46:09 +0530

    Changed next.config.ts &  vercel.json

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,16 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  rewrites: async () => [
-    {
-      source: '/api/auth/:path*',
-      destination: '/api/auth/:path*',
-    },
-    {
-      source: '/api/data/:path*',
-      destination: '/api/data/:path*',
-    },
-  ],
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: '/api/auth/:path*',
+      },
+      {
+        source: '/api/data/:path*',
+        destination: '/api/data/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
